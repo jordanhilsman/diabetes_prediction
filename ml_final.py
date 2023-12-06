2f8fe1887b2be6af9f1ffd733c5a5f91a13c867b
jordan <jordanhilsman@gmail.com>
2023-12-06 15:58:58 -0500

    Changes!

--- ml_final.py
+++ ml_final.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from sklearn.metrics import accuracy_score, precision_score, f1_score, recall_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold, cross_val_predict
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
@@ -12,9 +12,11 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 import sklearn.metrics.tests 
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
-from sklearn.feature_selection import SelectFromModel
-
-
+from imblearn.under_sampling import NearMiss
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.pipeline import Pipeline
 """
 For the purposes of this project, the target column Diabetes_012 is mapped:
     0: Healthy
@@ -23,6 +25,9 @@ For the purposes of this project, the target column Diabetes_012 is mapped:
 """
 
 df = pd.read_csv('diabetes_012_health_indicators_BRFSS2015.csv')
+
+#df = pd.read_csv('diabetes_binary_health_indicators_BRFSS2015.csv')
+
 
 dups = df[df.duplicated()]
 print("Number of dropped duplicate rows: ", len(dups))
@@ -33,35 +38,52 @@ df.drop_duplicates(inplace=True)
 
 #This is probably considered class imbalance, so we should do undersampling or something to address it.
 
+nm = NearMiss(version=1, n_neighbors=6)
+
+
+
 
 X = df.drop(columns=['Diabetes_012'])
 
 y = df['Diabetes_012']
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=1234)
+#X = df.drop(columns=['Diabetes_binary'])
 
-standardizer = StandardScaler().fit(X_train)
+#y = df['Diabetes_binary']
 
-X_train_st = standardizer.transform(X_train)
-X_test_st = standardizer.transform(X_test)
 
-selector = SelectFromModel(RandomForestClassifier(), threshold="median")
-selector.fit(X_train_st, y_train)
+X_under, y_under = nm.fit_resample(X,y)
 
-X_train_select = selector.transform(X_train_st)
-X_test_select = selector.transform(X_test_st)
 
-models = [KNeighborsClassifier(n_neighbors=5), GaussianNB(), 
+models = [KNeighborsClassifier(n_neighbors=6), GaussianNB(), 
         RandomForestClassifier()]
 
-for model in models:
-    print(f"Fitting {model}")
-    model.fit(X_train_select, y_train)
-    predictions = model.predict(X_test_select)
-    accuracy = accuracy_score(y_test, predictions)
-    precision = precision_score(y_test, predictions, average="weighted")
-    f1 = f1_score(y_test, predictions, average="weighted")
-    recall = recall_score(y_test, predictions, average="weighted")
-    print(f"Normalized Score for {model}: \n Accuracy: {accuracy:.2f} \n Precision: {precision:.2f} \n F1: {f1:.2f} \n Recall: {recall:.2f} \n")
+scoring_metrics = ['accuracy', 'precision_weighted', 'f1_weighted', 'roc_auc_ovr', 'recall_weighted']
 
+cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=1234)
+
+
+
+for model in models:
+    print(f"Evaluating {model} with 5-fold Cross Validation.")
+    pipeline = Pipeline([
+        ('scaler', StandardScaler()),
+        ('selector', SelectKBest(score_func=f_classif, k=15)),
+        ('model', model)
+        ])
+
+    y_pred = cross_val_predict(pipeline, X_under, y_under,  cv=cv)
+
+    for metric in scoring_metrics:
+        scores = cross_val_score(pipeline, X_under, y_under, cv=cv, scoring = metric)
+        print(f"{metric.capitalize()}: {scores.mean():.2f}")
+
+    cm = confusion_matrix(y_under, y_pred, labels = np.unique(y_under)
+                          )
+    disp = ConfusionMatrixDisplay(confusion_matrix
+                                  = cm, display_labels = np.unique( y_under)
+                                  )
+    disp.plot()
+    plt.title(f"Confusion Matrix for {model}")
+    plt.show()
